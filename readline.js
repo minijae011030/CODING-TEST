aa6b4f23f554810b50ec2d85d84a59fe2dfdf49a
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-20 10:13:02 +0900

    백준: 1260

--- readline.js
+++ readline.js
@@ -1,14 +1,3 @@
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-let input = [];
-
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  process.exit();
-});
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
