296da30598c0c971b18b5eaabff8bc45d5383cfe
Ian <105128049+minijae011030@users.noreply.github.com>
2025-05-23 21:37:36 +0900

    프로그래머스: 두 큐 합 같게 만들기 -> 코드 수정

--- PROGRAMMERS/두 큐 합을 같게 만들기/solution.js
+++ PROGRAMMERS/두 큐 합을 같게 만들기/solution.js
@@ -1,5 +1,6 @@
 function solution(queue1, queue2) {
   var answer = 0;
+  let n = queue1.length;
   let target =
     (queue1.reduce((acc, cur) => (acc += cur), 0) +
       queue2.reduce((acc, cur) => (acc += cur), 0)) /
@@ -8,22 +9,22 @@ function solution(queue1, queue2) {
   let copiedQueue1 = [...queue1];
   let copiedQueue2 = [...queue2];
 
-  let sum1 = copiedQueue1.reduce((acc, cur) => (acc += cur), 0);
+  let sum1 = queue1.reduce((acc, cur) => (acc += cur), 0);
   let queue1Idx = 0;
   let queue2Idx = 0;
 
   while (1) {
-    if (answer > queue1.length * 3) return -1;
+    if (answer > n * 3) return -1;
     if (sum1 === target) return answer;
 
     if (sum1 > target) {
-      let num1 = copiedQueue1[queue1Idx++];
-      copiedQueue2.push(num1);
+      let num1 = queue1[queue1Idx++];
+      queue2.push(num1);
       sum1 -= num1;
       answer++;
     } else {
-      let num2 = copiedQueue2[queue2Idx++];
-      copiedQueue1.push(num2);
+      let num2 = queue2[queue2Idx++];
+      queue1.push(num2);
       sum1 += num2;
       answer++;
     }
