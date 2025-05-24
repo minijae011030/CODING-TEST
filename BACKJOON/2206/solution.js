2701864427d3b0a26403ae032a123da91a51aba7
Ian <105128049+minijae011030@users.noreply.github.com>
2025-05-24 17:04:32 +0900

    백준: 2206

--- BACKJOON/2206/solution.js
+++ BACKJOON/2206/solution.js
@@ -5,17 +5,13 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 let idx = 0;
 let [N, M] = input[idx++].split(" ").map(Number);
 
-let map = Array.from({ length: N }, () => []);
+let map = [];
+let visited = Array.from({ length: N }, () =>
+  Array.from({ length: M }, () => [0, 0])
+);
 
 for (let i = 0; i < N; i++) {
-  map[i].push(...input[idx++].split("").map(Number));
-}
-
-let walls = [];
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < M; j++) {
-    if (map[i][j] == 1) walls.push([i, j]);
-  }
+  map.push(input[i + 1].split("").map(Number));
 }
 
 const direction = [
@@ -24,42 +20,30 @@ const direction = [
   [0, -1],
   [0, 1],
 ];
-let minDistance = Infinity;
-let visited = Array.from({ length: N }, () => Array(M).fill(0));
-let queue = [[0, 0, 1]];
-let start = 0;
 
-for (let k = 0; k < walls.length; k++) {
-  visited = Array.from({ length: N }, () => Array(M).fill(0));
-  queue = [[0, 0, 1]];
-  start = 0;
+let queue = [[0, 0, 0, 1]]; // x, y, used, distance
+visited[0][0][0] = 1;
 
-  let breakwall = walls[k];
-  map[breakwall[0]][breakwall[1]] = 0;
+let head = 0;
+while (queue.length > head) {
+  let [x, y, used, dist] = queue[head++];
 
-  while (start < queue.length) {
-    let [x, y, distance] = queue[start++];
+  if (x === N - 1 && y === M - 1) return console.log(dist);
+  for (let dir of direction) {
+    let [dx, dy] = [x + dir[0], y + dir[1]];
 
-    if (x == N - 1 && y == M - 1) {
-      minDistance = Math.min(minDistance, distance);
-      break;
+    if (dx < 0 || dy < 0 || dx >= N || dy >= M) continue;
+
+    if (map[dx][dy] === 0 && !visited[dx][dy][used]) {
+      visited[dx][dy][used] = 1;
+      queue.push([dx, dy, used, dist + 1]);
     }
 
-    for (let dir of direction) {
-      let [dx, dy] = [x + dir[0], y + dir[1]];
-      if (
-        dx >= 0 &&
-        dx < N &&
-        dy >= 0 &&
-        dy < M &&
-        !visited[dx][dy] &&
-        map[dx][dy] == 0
-      ) {
-        visited[dx][dy] = 1;
-        queue.push([dx, dy, distance + 1]);
-      }
+    if (map[dx][dy] === 1 && !used && !visited[dx][dy][1]) {
+      visited[dx][dy][1] = 1;
+      queue.push([dx, dy, 1, dist + 1]);
     }
   }
-  map[breakwall[0]][breakwall[1]] = 1;
 }
-minDistance == Infinity ? console.log(-1) : console.log(minDistance);
+
+return console.log(-1);
