4f99928ab2d85dd763843b7d437db1c82b90bac9
Ian <105128049+minijae011030@users.noreply.github.com>
2025-09-18 00:50:07 +0900

    백준: 1057

---
+++ BACKJOON/1057/solution.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let [N, A, B] = input[0].split(" ").map(Number);
+let cnt = 0;
+while (1) {
+  if (A === B) {
+    console.log(cnt);
+    return;
+  }
+
+  A = Math.floor((A + 1) / 2);
+  B = Math.floor((B + 1) / 2);
+
+  cnt++;
+}
