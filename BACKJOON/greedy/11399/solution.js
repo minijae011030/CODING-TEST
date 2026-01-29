645a4ab3ac640bc9a2541fe4a347c982b1172b2c
Ian <105128049+minijae011030@users.noreply.github.com>
2026-01-29 13:57:32 +0900

    백준: 1931

--- BACKJOON/greedy/11399/solution.js
+++ BACKJOON/greedy/11399/solution.js
@@ -2,10 +2,11 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const N = Number(input[0]);
-const arr = input[1].split(" ").map(Number);
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
 
 arr.sort((a, b) => a - b);
+
 let sum = 0;
-for (let i = 0; i < N; i++) sum += arr[i] * (N - i);
+arr.forEach((a, idx) => (sum += a * (n - idx)));
 console.log(sum);
--- BACKJOON/greedy/1931/solution.js
+++ BACKJOON/greedy/1931/solution.js
@@ -2,24 +2,22 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const n = Number(input[0]);
+let n = Number(input[0]);
+let arr = [];
+for (let i = 1; i <= n; i++) arr.push(input[i].split(" ").map(Number));
 
-let rooms = [];
-for (let i = 0; i < n; i++) {
-  rooms.push(input[i + 1].split(" ").map(Number));
-}
-rooms.sort((a, b) => {
+/**
+ * 가장 최선의 선택 -> 종료시간이 빠른거
+ * 종료시간이 똑같다? -> 시작시간이 빠른거
+ */
+arr.sort((a, b) => {
   if (a[1] === b[1]) return a[0] - b[0];
   return a[1] - b[1];
 });
 
-let endTime = rooms[0][1];
-let cnt = 1;
-for (let i = 1; i < rooms.length; i++) {
-  if (rooms[i][0] >= endTime) {
-    endTime = rooms[i][1];
-    cnt++;
-  }
+let a = [arr[0]];
+for (let i = 1; i < n; i++) {
+  if (a[a.length - 1][1] <= arr[i][0]) a.push(arr[i]);
 }
 
-console.log(cnt);
+console.log(a.length);
