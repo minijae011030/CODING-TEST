b66e043a14fb6c40a0366664d1eac74863690f0a
Ian <105128049+minijae011030@users.noreply.github.com>
2026-02-11 00:07:08 +0900

    백준: 1149

--- BACKJOON/dp/1149/solution.js
+++ BACKJOON/dp/1149/solution.js
@@ -2,21 +2,17 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const n = Number(input[0]);
-
-let cost = [];
-for (let i = 0; i < n; i++) {
-  cost.push(input[i + 1].split(" ").map(Number));
-}
-
+let n = Number(input[0]);
+let arr = [];
 let dp = Array.from({ length: n }, () => Array(3).fill(0));
 
-dp[0] = cost[0];
+for (let i = 1; i <= n; i++) arr.push(input[i].split(" ").map(Number));
+dp[0] = arr[0];
 
 for (let i = 1; i < n; i++) {
-  dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-  dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-  dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+  dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+  dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+  dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
 }
 
 console.log(Math.min(...dp[n - 1]));
