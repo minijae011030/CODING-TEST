2adf5048e3f3d9b555987df95d845ebf92e0830b
Ian <105128049+minijae011030@users.noreply.github.com>
2026-03-03 18:49:19 +0900

    프로그래머스: 멀리 뛰기

---
+++ PROGRAMMERS/dp/멀리 뛰기/solution.js
@@ -0,0 +1,10 @@
+function solution(n) {
+  let dp = Array.from({ length: n + 1 }).fill(0);
+
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+
+  return dp[n];
+}
