dd996f6b876a0eb247db5349c999f8bc29a352fc
Ian <105128049+minijae011030@users.noreply.github.com>
2026-03-23 18:15:12 +0900

    프로그래머스: 베스트앨범

--- PROGRAMMERS/data-structure/hash-map/베스트앨범/solution.js
+++ PROGRAMMERS/data-structure/hash-map/베스트앨범/solution.js
@@ -1,28 +1,56 @@
 function solution(genres, plays) {
-  var answer = [];
+  let n = genres.length;
 
-  let PlaysMap = new Map();
-  let SongsArray = [];
-
-  for (let i = 0; i < genres.length; i++) {
-    PlaysMap.set(genres[i], (PlaysMap.get(genres[i]) || 0) + plays[i]);
+  let musicInfoArr = [];
+  for (let i = 0; i < n; i++) {
+    musicInfoArr.push([i, genres[i], plays[i]]);
   }
-  let PlaysArray = [...PlaysMap].sort((a, b) => b[1] - a[1]);
 
-  for (let i = 0; i < genres.length; i++) {
-    SongsArray.push({ index: i, genre: genres[i], play: plays[i] });
+  // 1. 속한 노래가 많이 재생된 장르 찾기
+  let genresMap = new Map();
+  for (let i = 0; i < n; i++) {
+    let genre = genres[i];
+    let play = genresMap.get(genres[i]);
+
+    if (genresMap.has(genre)) {
+      genresMap.set(genre, play + plays[i]);
+    } else {
+      genresMap.set(genre, plays[i]);
+    }
   }
-  SongsArray.sort((a, b) => b.play - a.play);
 
-  PlaysArray.forEach(([genre, _]) => {
-    let cnt = 0;
-    for (let i = 0; i < SongsArray.length; i++) {
-      if (cnt < 2 && genre === SongsArray[i].genre) {
-        answer.push(SongsArray[i].index);
-        cnt++;
+  let genreArr = [...genresMap].sort((a, b) => b[1] - a[1]);
+
+  // 2. 장르 내에서 많이 재생된 노래 찾기 -> 고유 번호 낮은 노래 찾기
+  let album = [];
+  for (let i = 0; i < genreArr.length; i++) {
+    for (let j = 0; j < n; j++) {
+      if (genreArr[i][0] === musicInfoArr[j][1]) {
+        album.push(musicInfoArr[j]);
       }
     }
+  }
+
+  album.sort((a, b) => {
+    if (a[1] === b[1]) return b[2] - a[2];
   });
+
+  // 3. 장르별로 두개씩 모아 수록하기
+  let answer = [];
+  let cnt = 0;
+  let curAlbum = album[0][1];
+  for (let i = 0; i < n; i++) {
+    if (curAlbum !== album[i][1]) {
+      answer.push(album[i][0]);
+      cnt = 1;
+      curAlbum = album[i][1];
+    } else {
+      cnt++;
+      if (cnt < 3) {
+        answer.push(album[i][0]);
+      }
+    }
+  }
 
   return answer;
 }
