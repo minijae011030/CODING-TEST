a2a89e269e035d20bf955c2b91504c22fbd0cfc5
Ian <105128049+minijae011030@users.noreply.github.com>
2026-05-11 22:08:17 +0900

    프로그래머스: 양과 늑대

--- PROGRAMMERS/implementation/backtracking/양과 늑대/solution.js
+++ PROGRAMMERS/implementation/backtracking/양과 늑대/solution.js
@@ -1,23 +1,47 @@
 function solution(info, edges) {
+  /*
+    양과 늑대
+    
+    이진트리모양 노드에 늑대와 양 한마리씩
+    루트에서 출발하여 각 노드를 돌아다니며 양을 모으려고 함
+    노드 방문시 해당 노드의 양과 늑대가 나를 따라옴
+    늑대는 양을 잡아먹으려고 함
+    양의 수 <= 늑대의 수 -> 모든 양이 잡아먹힘
+    중간에 양이 잡아먹히지 않도록하면서 최대한 많은 수의 양을 모으고 루트로 돌아오기
+    모을 수 있는 양의 최대 수 return
+    
+    2 <= info.length <= 17
+    info는 0 or 1
+    0 양, 1 늑대
+    info[0] 항상 양
+    
+    edges의 각 행은 [부모느도번호, 자식노드번호] => 서로 연결된 두 노드를 나타냄
+    0번 노드는 항상 루트
+    */
+
   let answer = [];
   let visited = new Array(info.length).fill(0);
 
-  function DFS(sheep, wolf) {
-    if (sheep > wolf) answer.push(sheep);
-    else return;
+  function dfs(wolf, sheep) {
+    answer.push(sheep);
 
-    for (let [p, c] of edges) {
-      if (visited[p] && !visited[c]) {
-        visited[c] = 1;
-        if (info[c] === 0) DFS(sheep + 1, wolf);
-        else DFS(sheep, wolf + 1);
-        visited[c] = 0;
+    for (let [parent, child] of edges) {
+      if (visited[parent] && !visited[child]) {
+        visited[child] = 1;
+
+        let isWolf = info[child];
+
+        let nsheep = sheep + (isWolf ? 0 : 1);
+        let nwolf = wolf + (isWolf ? 1 : 0);
+
+        if (nsheep > nwolf) dfs(nwolf, nsheep);
+
+        visited[child] = 0;
       }
     }
   }
 
   visited[0] = 1;
-  DFS(1, 0);
-
+  dfs(0, 1);
   return Math.max(...answer);
 }
